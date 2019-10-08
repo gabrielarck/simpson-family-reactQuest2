b58af8cd0092f687270b49c41063654c3bcf6571
gabrielarck <stanislasvega@yahoo.fr>
2019-10-08 09:37:53 +0200

    intro

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar(){
+    return (
+        <nav className="Navbar">
+            <a 
+                href="https://fr.wikipedia.org/wiki/Les_Simpson">
+                Wiki des Simpsons
+            </a>
+        </nav>
+    )
+}
+
+export default Navbar;
