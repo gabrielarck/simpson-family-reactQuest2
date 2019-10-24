5b0272a184b63c7e40c46554ee20c746f9ba5cb0
gabrielarck <stanislasvega@yahoo.fr>
2019-10-24 11:23:04 +0200

    modify fils

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
 import QuoteList from './components/QuoteList';
+import QuoteForm from './components/QuoteForm';
 
 function App() {
   return (
    
     <div className="App">
       <header className="App-header">
-        <Navbar/>   
+        <Navbar/> 
+        <QuoteForm/> 
         <QuoteCard character="Nelson Muntz" alt="Nelson Muntz" image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"/>
         <QuoteCard character="Bart" alt="Bart" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOrXf0sDmW8T70086M7oikd6w1ANVgaqSGe4QRjv_Cq0NjU3dx"/>
         <QuoteList/>
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -2,35 +2,34 @@ import React from 'react';
 import './QuoteCard.css';
 
 class QuoteCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            favorite: false
-        };
-    }
-    render() {
-        return (
-            <figure className="QuoteCard">
-                <img className="img" src={this.props.image} alt={this.props.alt}/>
-                <figcaption>
-                    <blockquote className="blockquote">
-                        Shoplifting is a victimless crime, like punching someone in the dark.
-                    </blockquote>
-                    <p>
-                        <cite>{this.props.character}</cite>
-                        
-                        <span
-                            className={this.state.favorite ? "is-favorite" : ""}
-                            onClick={event => {
-                                const newFavorite =! this.state.favorite;
-                                this.setState({ favorite: newFavorite });
-                            }}
-                            >&#9733;</span>
-                    </p>
-                </figcaption>
-            </figure>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: false,
     };
+  }
+  render() {
+    return (
+      <figure className="QuoteCard">
+        <img src={this.props.image} alt={this.props.character} />
+        <figcaption>
+          <blockquote>{this.props.quote}</blockquote>
+          <p>
+            <cite>{this.props.character}</cite>
+            <span
+              className={this.state.favorite ? 'is-favorite' : ''}
+              onClick={event => {
+                const newFavorite = !this.state.favorite;
+                this.setState({ favorite: newFavorite });
+              }}
+            >
+              &#9733;
+            </span>
+          </p>
+        </figcaption>
+      </figure>
+    );
+  }
 }
 
 export default QuoteCard;
