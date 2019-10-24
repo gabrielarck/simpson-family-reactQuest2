bc1cfaea00c5be2f00544a2d4fa42e66fdd57b43
gabrielarck <stanislasvega@yahoo.fr>
2019-10-24 11:38:50 +0200

    modif css

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './QuoteForm.css';
 
+
+const MAX_LENGTH = 30; // You can change the value
 class QuoteForm extends Component {
   constructor(props) {
     super(props);
@@ -13,22 +15,30 @@ class QuoteForm extends Component {
   handleSubmit(event) {
     event.preventDefault();
   }
-
   handleChange(event) {
+  if (event.target.value.length > MAX_LENGTH){
+    return;
+  }
     this.setState({ character: event.target.value });
   }
 
   render() {
+    const maximumReached = this.state.character.length >= MAX_LENGTH;
+    const numRemaining = MAX_LENGTH - this.state.character.length;
     return (
       <form className="QuoteForm" onSubmit={this.handleSubmit}>
         <label htmlFor="character">Character:</label>
         <input
+        className={maximumReached ? 'length-maximum-reached' : 'length-ok'}
           id="name"
           name="character"
           type="text"
           value={this.state.character}
           onChange={this.handleChange}
         />
+        <small className="remaining-characters">
+          {numRemaining} remaining characters
+        </small>
         <p>
           You typed: <strong>{this.state.character}</strong>
         </p>
